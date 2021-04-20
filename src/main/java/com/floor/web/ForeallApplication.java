d6bede7a06d8c9054536c84f3976ca9a60c2d732
Danil Mazurantov <mrzdanil@gmail.com>
2021-04-20 13:48:46 +0300

    initProject

---
+++ src/main/java/com/floor/web/ForeallApplication.java
@@ -0,0 +1,13 @@
+package com.floor.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForeallApplication.class, args);
+	}
+
+}
